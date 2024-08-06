bef505d29fe90665f0ef4b894d12a7b2b905f855
hlokomani-tuks <133780278+hlokomani-tuks@users.noreply.github.com>
2024-08-06 12:20:47 +0200

    Added lottie as an allowed host

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'lottie.host',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  }
 
 export default nextConfig;
